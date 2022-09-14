2b4adfca32be8f9552b38a12292fe198e7c536fe
markbrauning <markbrauning@gmail.com>
2022-09-13 22:02:43 -0500

    okay

--- V4_Code.py
+++ V4_Code.py
@@ -112,33 +112,7 @@ def read_in_gaits():
                     row_count += 1
                 print('Gait 3. Processed %s rows.' % (row_count))
 
-#Desc.
-def controller_main(controller):
-	print("controller_main thread has started.")
-	global psc
-	while True:
-		time.sleep(0.2)
-		events = pygame.event.get()
-		psc = controller_inputs(controller)
-	print("End of controller_main code.")
 
-#Desc.
-class controller_inputs():
-	def __init__(self,controller):
-		accuracy = 3
-		self.controller = controller
-		events = pygame.event.get()
-		self.j_lx = round(controller.get_axis(0),accuracy)
-		self.j_ly = round(controller.get_axis(1),accuracy)
-		self.j_rx = round(controller.get_axis(2),accuracy)
-		self.j_ry = round(controller.get_axis(3),accuracy)
-		self.b_select = controller.get_button(8)
-		self.b_start = controller.get_button(9)
-		self.b_ps = controller.get_button(10)
-		self.b_cross = controller.get_button(0)
-		self.b_circle = controller.get_button(1)
-		self.b_triangle = controller.get_button(2)
-		self.b_square = controller.get_button(3)
 
 #Desc.
 def foot_vector_main():
@@ -180,30 +154,34 @@ def calc_foot_vectors(Qx,Qy,Qr):
 			
 		Qrx = g_datum[leg][0]*math.cos(body_rotation) - g_datum[leg][1]*math.sin(body_rotation)
 		Qry = g_datum[leg][0]*math.sin(body_rotation) + g_datum[leg][1]*math.cos(body_rotation)
-
+		
 		gfv[leg][0]= (Qx + Qrx)
 		gfv[leg][1]= (Qy + Qry)
-
+		
 		foot_vectors[leg][0] = round((gfv[leg][0]-g_datum[leg][0])*math.cos(xlation_angle[leg]) - (gfv[leg][1]-g_datum[leg][1])*math.sin(xlation_angle[leg]),2)
 		foot_vectors[leg][1] = round((gfv[leg][0]-g_datum[leg][0])*math.sin(xlation_angle[leg]) + (gfv[leg][1]-g_datum[leg][1])*math.cos(xlation_angle[leg]),2)
 		foot_vectors[leg][2] = round(((foot_vectors[leg][0]**2)+(foot_vectors[leg][1]**2))**(0.5),2)
-	
 	return foot_vectors
+
+#Set up min max pulse of servos
+def motor_calibration():
+    x=1
+    #future
+    #use_pwm = int(servo_config[use_col][use_angle])
+    #kit.servo[0].set_pulse_width_range(1000, 2000)
+    #use_pwm = int(servo_config[use_col][use_angle])
 
 #Take in calucluated angles for a motor and moves it using PWM
 def move_motor(leg, part, servo_angle):
-        use_col = 3*leg + part
-        use_i2c = int(servo_config[use_col][183])
-        use_channel = int(servo_config[use_col][184])
-        use_angle = int(servo_angle)
+    use_col = 3*leg + part
+    use_i2c = int(servo_config[use_col][183])
+    use_channel = int(servo_config[use_col][184])
+    use_angle = int(servo_angle)
 
-        use_pwm = int(servo_config[use_col][use_angle])
-
-        
-        if use_i2c == 0:
-                pwm0.setPWM(use_channel, 0, use_pwm)
-        else:
-                pwm1.setPWM(use_channel, 0, use_pwm)
+    if use_i2c == 0:
+        pwm0.servo[use_channel].angle = use_angle
+    else:
+        pwm1.servo[use_channel].angle = use_angle
                 
 #Takes in the current angles and moves each leg with the "move_motor" function
 def move_legs(servo_angles):
@@ -213,107 +191,123 @@ def move_legs(servo_angles):
             
 #Takes in the desired XYZ components for each leg and outputs the end angles
 def calc_angles(foot_coordinates):
-        l1 = 7.75
-        l2 = 14.5
-        l3 = 5.75
+    l1 = 7.75
+    l2 = 14.5
+    l3 = 5.75
+    
+    #                leg1      2       3       4       5       6
+    #           [beta,theta,alpha] ie shoulder,arm,claw
+    servo_angles = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+
+
+    for leg in range(6):
+        x = foot_coordinates[leg][0]
+        y = foot_coordinates[leg][1]
+        z = foot_coordinates[leg][2]
+        xy = ((x**2)+(y**2))**(0.5)
+        c = (((xy-l3)**2)+(z**2))**(0.5)
+
+        #Calculate alpha
+        #fix acos domain error
+        alpha_acos_piece = ((l1**2)+(l2**2)-(c**2))/(2*l1*l2)
+
+        if (alpha_acos_piece<=-1):
+                alpha_acos_piece = -1
+        if (alpha_acos_piece>=1):
+                alpha_acos_piece = 1
+
+        alpha = math.acos(alpha_acos_piece) - (math.pi/2)
+                
+        #Calculate theta
+        #fix acos domain error
+        theta_acos_piece = ((l1**2)+(c**2)-(l2**2))/(2*l1*c)
+        if (theta_acos_piece<=-1):
+                theta_acos_piece = -1
+        if (theta_acos_piece>=1):
+                theta_acos_piece = 1
         
-        #                leg1      2       3       4       5       6
-        #           [beta,theta,alpha] ie shoulder,arm,claw
-        servo_angles = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+        theta_acos_piece1 = z/c
+        if (theta_acos_piece1<=-1):
+                theta_acos_piece1 = -1
+        if (theta_acos_piece1>=1):
+                theta_acos_piece1 = 1
+        
+        if(c == 0):
+                c = 0.00000000001
 
+        if(xy<=l3):
+                theta_piece1 = - math.acos(theta_acos_piece1)
+        else:
+                theta_piece1 = math.acos(theta_acos_piece1)
+        
+        theta = theta_piece1 - math.acos(theta_acos_piece)
+        
+        #Calculate beta
+        beta_acos_piece = x/xy
+        if (beta_acos_piece<=-1):
+                beta_acos_piece = -1
+        if (beta_acos_piece>=1):
+                beta_acos_piece = 1
+        if(x == 0):
+                x = 0.00000000001
+        beta = math.pi - math.acos(beta_acos_piece)
+        
+        if (beta > math.pi):
+                beta = math.pi
+        if (alpha > math.pi):
+                alpha = math.pi
+        if (theta > math.pi):
+                theta = math.pi
+        
+        if (beta < 0):
+                beta = 0
+        if (alpha < 0):
+                alpha = 0
+        if (theta < 0):
+                theta = 00
 
-        for leg in range(6):
-                x = foot_coordinates[leg][0]
-                y = foot_coordinates[leg][1]
-                z = foot_coordinates[leg][2]
-                xy = ((x**2)+(y**2))**(0.5)
-                c = (((xy-l3)**2)+(z**2))**(0.5)
-
-                #Calculate alpha
-                #fix acos domain error
-                alpha_acos_piece = ((l1**2)+(l2**2)-(c**2))/(2*l1*l2)
-
-                if (alpha_acos_piece<=-1):
-                        alpha_acos_piece = -1
-                if (alpha_acos_piece>=1):
-                        alpha_acos_piece = 1
-
-                alpha = math.acos(alpha_acos_piece) - (math.pi/2)
-                        
-                #Calculate theta
-                #fix acos domain error
-                theta_acos_piece = ((l1**2)+(c**2)-(l2**2))/(2*l1*c)
-                if (theta_acos_piece<=-1):
-                        theta_acos_piece = -1
-                if (theta_acos_piece>=1):
-                        theta_acos_piece = 1
-                
-                theta_acos_piece1 = z/c
-                if (theta_acos_piece1<=-1):
-                        theta_acos_piece1 = -1
-                if (theta_acos_piece1>=1):
-                        theta_acos_piece1 = 1
-                
-                if(c == 0):
-                        c = 0.00000000001
-
-                if(xy<=l3):
-                        theta_piece1 = - math.acos(theta_acos_piece1)
-                else:
-                        theta_piece1 = math.acos(theta_acos_piece1)
-                        
-                theta = theta_piece1 - math.acos(theta_acos_piece)
-
-                #Calculate beta
-                beta_acos_piece = x/xy
-                if (beta_acos_piece<=-1):
-                        beta_acos_piece = -1
-                if (beta_acos_piece>=1):
-                        beta_acos_piece = 1
-                if(x == 0):
-                        x = 0.00000000001
-                beta = math.pi - math.acos(beta_acos_piece)
-                
-                if (beta > math.pi):
-                        beta = math.pi
-                if (alpha > math.pi):
-                        alpha = math.pi
-                if (theta > math.pi):
-                        theta = math.pi
-                
-                if (beta < 0):
-                        beta = 0
-                if (alpha < 0):
-                        alpha = 0
-                if (theta < 0):
-                        theta = 00
-
-                servo_angles[leg] = [beta*(180/math.pi),theta*(180/math.pi),alpha*(180/math.pi)]
-                
-        return servo_angles
+        servo_angles[leg] = [beta*(180/math.pi),theta*(180/math.pi),alpha*(180/math.pi)]
+        
+    return servo_angles
 
 #Desc.
 def calc_foot_xyz(i):
-        x_datum = 0
-        y_datum = 5
-        z_datum = -19
-        scale = 2
-        #                      leg1     2       3       4       5       6
-        #                    [x,y,z] [x,y,z] [x,y,z] [x,y,z] [x,y,z] [x,y,z]
-        foot_coordinates  = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        
-        for leg in range(6):
-                #access z column of gait csv and multiply it by the magnitude of the foot vector for that leg
-                foot_coordinates[leg][2] = float(gait_array[gait_selected][leg*2][i])*foot_vectors[leg][2]*scale + z_datum
-                
-                #access the xy column of the gait csv and multiply it by the x component of the foot vector
-                foot_coordinates[leg][0] = float(gait_array[gait_selected][leg*2+1][i])*foot_vectors[leg][0]*scale + x_datum
-                
-                #access the xy column of the gait csv and multiply it by the y component of the foot vector
-                foot_coordinates[leg][1] = float(gait_array[gait_selected][leg*2+1][i])*foot_vectors[leg][1]*scale + y_datum
-        
-        return foot_coordinates
+    x_datum = 0
+    y_datum = 5
+    z_datum = -19
+    scale = 2
+    #                      leg1     2       3       4       5       6
+    #                    [x,y,z] [x,y,z] [x,y,z] [x,y,z] [x,y,z] [x,y,z]
+    foot_coordinates  = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
+    for leg in range(6):
+            #access z column of gait csv and multiply it by the magnitude of the foot vector for that leg
+            foot_coordinates[leg][2] = float(gait_array[gait_selected][leg*2][i])*foot_vectors[leg][2]*scale + z_datum
+            
+            #access the xy column of the gait csv and multiply it by the x component of the foot vector
+            foot_coordinates[leg][0] = float(gait_array[gait_selected][leg*2+1][i])*foot_vectors[leg][0]*scale + x_datum
+            
+            #access the xy column of the gait csv and multiply it by the y component of the foot vector
+            foot_coordinates[leg][1] = float(gait_array[gait_selected][leg*2+1][i])*foot_vectors[leg][1]*scale + y_datum
+    return foot_coordinates
+
+#Desc.
+class controller_inputs():
+	def __init__(self,controller):
+		accuracy = 3
+		self.controller = controller
+		events = pygame.event.get()
+		self.j_lx = round(controller.get_axis(0),accuracy)
+		self.j_ly = round(controller.get_axis(1),accuracy)
+		self.j_rx = round(controller.get_axis(2),accuracy)
+		self.j_ry = round(controller.get_axis(3),accuracy)
+		self.b_select = controller.get_button(8)
+		self.b_start = controller.get_button(9)
+		self.b_ps = controller.get_button(10)
+		self.b_cross = controller.get_button(0)
+		self.b_circle = controller.get_button(1)
+		self.b_triangle = controller.get_button(2)
+		self.b_square = controller.get_button(3)
 #Desc.
 def gait_main():
 	print("gait_main thread has started.")
